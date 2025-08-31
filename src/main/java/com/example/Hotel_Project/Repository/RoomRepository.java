2f08b50454cdee15263f2d84bd91c3e4e5ab90d4
JooSH001 <heyjoo01@hanmail.net>
2025-08-31 17:57:32 +0900

    Update RoomRepository.java

--- src/main/java/com/example/Hotel_Project/Repository/RoomRepository.java
+++ src/main/java/com/example/Hotel_Project/Repository/RoomRepository.java
@@ -2,9 +2,17 @@ package com.example.Hotel_Project.Repository;
 
 import com.example.Hotel_Project.Entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import jakarta.persistence.LockModeType;
 
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Integer> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select r from Room r where r.id = :id")
+    Optional<Room> findByIdForUpdate(@Param("id") Integer id);
 
 }
