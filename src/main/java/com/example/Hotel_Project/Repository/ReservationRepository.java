ce830985daf9f1f64629ce6161738547ff396cde
JooSH001 <heyjoo01@hanmail.net>
2025-08-31 19:31:15 +0900

    Update ReservationRepository.java

--- src/main/java/com/example/Hotel_Project/Repository/ReservationRepository.java
+++ src/main/java/com/example/Hotel_Project/Repository/ReservationRepository.java
@@ -2,6 +2,7 @@ package com.example.Hotel_Project.Repository;
 
 import com.example.Hotel_Project.Entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
