f1b72ea5ff564d941352dd69027345b6dac60ae0
JooSH001 <heyjoo01@hanmail.net>
2025-09-01 13:57:59 +0900

    Update RoomRepository.java

--- src/main/java/com/example/Hotel_Project/Repository/RoomRepository.java
+++ src/main/java/com/example/Hotel_Project/Repository/RoomRepository.java
@@ -3,6 +3,7 @@ package com.example.Hotel_Project.Repository;
 import com.example.Hotel_Project.Entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
